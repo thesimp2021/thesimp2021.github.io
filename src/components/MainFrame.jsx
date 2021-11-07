be51e12dbc8fedc9b07914d4bc38184a6537e0a6
thesimp2021 <ultimateaqy@gmail.com>
2021-11-07 22:26:13 +0700

    yauda

--- src/components/MainFrame.jsx
+++ src/components/MainFrame.jsx
@@ -23,94 +23,6 @@ const renderAssets = (assets) => {
   );
 };
 
-const renderShopModal = (props) => {
-    const {
-        money,
-        setMoney,
-        assets,
-        setAssets,
-        purchaseText,
-        setPurchaseText,
-        isShopModalOpen,
-        setShopModalOpen
-    } = props;
-
-    const handleBuyClick = (price, itemId) => {
-        setMoney(prevMoney => prevMoney - price);
-        setAssets(prevAssets => [...prevAssets, itemId]);
-        setPurchaseText(purchaseTextDatabase[itemId]);
-    };
-
-    return (
-        <Modal
-            show={isShopModalOpen}
-            onHide={() => {
-                setPurchaseText('');
-                setShopModalOpen(false);
-            }}
-            size='xl'
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-        >
-            <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    Shop 🏪 {purchaseText !== '' && <span id='purchase-text'><b>"{purchaseText}"</b></span>}
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <div className='item-list'>
-                    {itemsDatabase.filter(item => item.id === 0 || item.id === 1 || item.id === 2).map(item => {
-                        const isDisabled = assets.includes(item.id) || money < item.price;
-
-                        return (
-                            <Card className='item-card'>
-                                <Card.Header>{item.name} ${item.price}</Card.Header>
-                                <Card.Body>
-                                    <img className='item-photo' src={item.imageUrl} />
-                                    <div id='purchase-action-button'>
-                                        <Button
-                                            disabled={isDisabled}
-                                            variant='success'
-                                            onClick={() => handleBuyClick(item.price, item.id)}
-                                            size='sm'
-                                        >
-                                            purchase item
-                                        </Button>
-                                    </div>
-                                </Card.Body>
-                            </Card>
-                        );
-                    })}
-                </div>
-                <div className='item-list'>
-                    {itemsDatabase.filter(item => item.id === 3 || item.id === 4).map(item => {
-                        const isDisabled = assets.includes(item.id) || money < item.price;
-
-                        return (
-                            <Card className='item-card'>
-                                <Card.Header>{item.name} ${item.price}</Card.Header>
-                                <Card.Body>
-                                    <img className='item-photo' src={item.imageUrl} />
-                                    <div id='purchase-action-button'>
-                                        <Button
-                                            disabled={isDisabled}
-                                            variant='success'
-                                            onClick={() => handleBuyClick(item.price, item.id)}
-                                            size='sm'
-                                        >
-                                            purchase item
-                                        </Button>
-                                    </div>
-                                </Card.Body>
-                            </Card>
-                        );
-                    })}
-                </div>
-            </Modal.Body>
-        </Modal>
-    );
-}
-
 const MainFrame = (props) => {
     const playerName = _.get(props, 'location.state.playerName', '');
     const playerTitleId = _.get(props, 'location.state.playerTitleId', 0);
@@ -121,7 +33,130 @@ const MainFrame = (props) => {
     const [assets, setAssets] = useState([]);
     const [money, setMoney] = useState(50000);
 
-    const renderIndidualLevel = (levelInfo) => {
+    const [isQuizModalOpen, setIsQuizModalOpen] = useState(false);
+    const [unlockedLevel, setUnlockedLevel] = useState(-1);
+    const [isCurrentLevelAllAnswered, setIsCurrentLevelAllAnswered] = useState(false);
+    const [isCurrentQuestionAnswered, setIsCurrentQuestionAnswered] = useState(false);
+    const [currentQuestionId, setCurrentQuestionId] = useState(0);
+    const [totalCorrectAnswers, setTotalCorrectAnswers] = useState(0);
+
+    //di final display, correct answers - 5 (soale ada bonus questions di tiap level)
+
+    const renderShopModal = () => {
+        const handleBuyClick = (price, itemId) => {
+            setMoney(prevMoney => prevMoney - price);
+            setAssets(prevAssets => [...prevAssets, itemId]);
+            setPurchaseText(purchaseTextDatabase[itemId]);
+            setUnlockedLevel(prevUnlockedLevel => prevUnlockedLevel+1);
+            setIsCurrentLevelAllAnswered(false);
+        };
+
+        return (
+            <Modal
+                show={isShopModalOpen}
+                onHide={() => {
+                    setPurchaseText('');
+                    setShopModalOpen(false);
+                }}
+                size='xl'
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Shop 🏪 {purchaseText !== '' && <span id='purchase-text'><b>"{purchaseText}"</b></span>}
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <div className='item-list'>
+                        {itemsDatabase.filter(item => item.id === 0 || item.id === 1 || item.id === 2).map(item => {
+                            const isDisabled = assets.includes(item.id) || money < item.price;
+
+                            return (
+                                <Card className='item-card'>
+                                    <Card.Header>{item.name} ${item.price}</Card.Header>
+                                    <Card.Body>
+                                        <img className='item-photo' src={item.imageUrl} />
+                                        <div id='purchase-action-button'>
+                                            <Button
+                                                disabled={isDisabled}
+                                                variant='success'
+                                                onClick={() => handleBuyClick(item.price, item.id)}
+                                                size='sm'
+                                            >
+                                                purchase item
+                                            </Button>
+                                        </div>
+                                    </Card.Body>
+                                </Card>
+                            );
+                        })}
+                    </div>
+                    <div className='item-list'>
+                        {itemsDatabase.filter(item => item.id === 3 || item.id === 4).map(item => {
+                            const isDisabled = assets.includes(item.id) || money < item.price;
+
+                            return (
+                                <Card className='item-card'>
+                                    <Card.Header>{item.name} ${item.price}</Card.Header>
+                                    <Card.Body>
+                                        <img className='item-photo' src={item.imageUrl} />
+                                        <div id='purchase-action-button'>
+                                            <Button
+                                                disabled={isDisabled}
+                                                variant='success'
+                                                onClick={() => handleBuyClick(item.price, item.id)}
+                                                size='sm'
+                                            >
+                                                purchase item
+                                            </Button>
+                                        </div>
+                                    </Card.Body>
+                                </Card>
+                            );
+                        })}
+                    </div>
+                </Modal.Body>
+            </Modal>
+        );
+    }
+
+    const renderQuizModal = () => {
+        const renderFinishLevelButton = () => {
+            return (
+                <Button
+                    onClick={
+                        () => {
+                            setIsQuizModalOpen(false);
+                            setUnlockedLevel(unlockedLevel + 1);
+                            setIsCurrentQuestionAnswered(false);
+                            setCurrentQuestionId(0);
+                            setIsCurrentLevelAllAnswered(true);
+                        }
+                    }
+                >
+                    Finish Level
+                </Button>
+            )
+        };
+
+        const renderNextQuestionButton = () => {
+            return (
+                <Button
+                    onClick={() => {
+                        setIsCurrentQuestionAnswered(false);
+                        setCurrentQuestionId(currentQuestionId+1);
+                    }}
+                >
+                    Finish Level
+                </Button>
+            )
+        };
+
+        return
+    }
+
+    const renderIndidualLevel = (levelInfo,levelIndex) => {
         return (
             <Card bg='dark' text='white' className='level-cards'>
                 <Card.Header>
@@ -133,7 +168,12 @@ const MainFrame = (props) => {
                         <Button size='sm'>
                             ⓘ
                         </Button>
-                        <Button size='sm' variant='danger'>
+                        <Button
+                            disabled={unlockedLevel !== levelIndex || isCurrentLevelAllAnswered}
+                            onClick={() => setIsCurrentLevelAllAnswered(true)}
+                            size='sm'
+                            variant='danger'
+                        >
                             hayuk atuh!
                         </Button>
                     </div>
@@ -164,23 +204,14 @@ const MainFrame = (props) => {
                                 JAJAN ATUH!
                             </Button>
                         </div>
-                        {renderShopModal(
-                            {
-                                money,
-                                setMoney,
-                                assets,
-                                setAssets,
-                                purchaseText,
-                                setPurchaseText,
-                                isShopModalOpen,
-                                setShopModalOpen
-                            }
-                        )}
+                        {renderShopModal()}
                     </Col>
                     <Col lg={10} md={10} sm={10} xl={10} xs={10} xxl={10}>
                         <Row>
                             <div className='level-list'>
-                                {levelsDatabase.map(individualLevel => renderIndidualLevel(individualLevel))}
+                                {levelsDatabase.map(
+                                    (individualLevel, index) => renderIndidualLevel(individualLevel, index)
+                                )}
                             </div>
                         </Row>
                     </Col>
